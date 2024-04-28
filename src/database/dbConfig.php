957f9350cdd66304b9452184a7b46b8af0850e43
eliseu daniel <eliseudaniel29@gmail.com>
2024-04-28 15:19:14 -0300

    config database

--- src/database/dbConfig.php
+++ src/database/dbConfig.php
@@ -1,2 +1,25 @@
+<?php
+
+namespace src\database;
+
+use PDO;
+
+class Config{
+
+    private $userName = 'root';
+    private $passWord = '';
+    private $dbName = 'ProAcademicHub';
+    private $host = '127.0.0.1';
+    
+    public function query(string $sql){
+        $connection = new PDO("mysql:host={$this->host};dbname={$this->dbName}", $this->userName, $this->password);
+        $preparation = $connection->prepare($sql);
+        $preparation->execute();   
+        
+        return $preparation->fetchAll(PDO::FETCH_OBJ);
+    }
 
 
+
+
+}
